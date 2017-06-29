b430f0d1817e19c8edaf388d5948dad1451efbdc
cobusc <cobus.carstens@gmail.com>
2017-06-29 13:05:45 +0200

    URL ordering changed so that longer paths with the same prefixes as others will appear first, i.e. most specific matches are listed first

--- templates/urls.py
+++ templates/urls.py
@@ -7,7 +7,8 @@ from django.conf.urls import url
 import {{ module }}.views as views
 
 urlpatterns = [
-    {% for relative_url, class_name in entries|dictsort(true) %}
+    {# URLs are traverse in reversed sorted order so that longer ones are evaluated first #}
+    {% for relative_url, class_name in entries|dictsort(true)|reverse %}
     url(r"^{{ relative_url }}$", views.{{ class_name }}.as_view()),
     {% endfor %}
 ]
