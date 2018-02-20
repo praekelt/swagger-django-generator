12c89c57a37707d2ec0b2418753668515c7d138f
cobusc <cobus.carstens@gmail.com>
2018-02-20 15:01:22 +0200

    Make UI inclusion optional

--- swagger_django_generator/templates/aiohttp/urls.py
+++ swagger_django_generator/templates/aiohttp/urls.py
@@ -5,11 +5,11 @@ Routing module.
 """
 import {{ module }}.views as views
 
-def add_routes(app):
+def add_routes(app, with_ui=False):
     {# URLs are traverse in reversed sorted order so that longer ones are evaluated first #}
     {% for relative_url, class_name in entries|dictsort(true)|reverse %}
     app.router.add_view(r"/{{ relative_url }}", views.{{ class_name }})
     {% endfor %}
-    # if settings.DEBUG:
-    app.router.add_view(r"/the_specification", views.__SWAGGER_SPEC__)
-    app.router.add_static(r"/ui", path="ui")
+    if with_ui:
+        app.router.add_view(r"/the_specification", views.__SWAGGER_SPEC__)
+        app.router.add_static(r"/ui", path="ui")
